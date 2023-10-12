f0e78f9b1eb405c2bf3336ad97f2df2b8d63dd69
François Vasseur <vasseurfrancois11@gmail.com>
2023-10-12 17:48:13 +0200

    Receive request to update db

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const sqlite3 = require("sqlite3");
 const cors = require("cors");
-const app = express()
+const bodyParser = require("body-parser");
+const app = express();
 
 const port = 3000;
 const db = new sqlite3.Database("accommodations.db");
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.send("API !");
@@ -36,4 +38,24 @@ app.get("/api/airbnb-database/:id", (req, res) => {
 
 app.listen(port, () => {
     console.log(`Serveur écoutant sur le port ${port}`);
+});
+
+
+app.put("/api/airbnb-database/:id", (req, res) => {
+    const id = req.params.id;
+    // données de la requête
+    const newData = req.body;
+
+    // Mise à jour de la base de données
+    db.run(
+        "UPDATE accommodations SET favourite = ? WHERE id = ?",
+        [newData.favourite, id],
+        (err) => {
+            if (err) {
+                res.status(500).json({ error: "Erreur lors de la mise à jour" });
+            } else {
+                res.json({ message: "Mise à jour réussie" });
+            }
+        }
+    );
 });
